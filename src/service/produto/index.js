f2c35ac82f1752b2f7b51a2bd9f331deeec08654
janderson.silva <janderson.silva@rivendel.com.br>
2019-02-16 22:31:10 -0200

    Removido o mock de produtos do ProdutoService. Alterado a estrutura de exportação do arquivo. Conectado a camada de serviço com o repositório de produto.

--- src/service/produto/index.js
+++ src/service/produto/index.js
@@ -1,65 +1,31 @@
-const produtos = [
-  {
-      id: 1,
-      title: 'Item vindo da api',
-      description: 'Descrição do Item 1',
-      image: 'sofa.jpg',
-  },
-  {
-      id: 2,
-      title: 'Item 2',
-      description: 'Descrição do Item 2',
-      image: 'sofa.jpg',
-  },
-  {
-      id: 3,
-      title: 'Item 3',
-      description: 'Descrição do Item 3',
-      image: 'sofa.jpg',
-  },
-  {
-      id: 4,
-      title: 'Item 4',
-      description: 'Descrição do Item 4',
-      image: 'sofa.jpg',
-  },
-  {
-      id: 5,
-      title: 'Item 5',
-      description: 'Descrição do Item 5',
-      image: 'sofa.jpg',
-  },
-  {
-      id: 6,
-      title: 'Item 6',
-      description: 'Descrição do Item 6',
-      image: 'sofa.jpg',
-  },
-  {
-      id: 7,
-      title: 'Item 7',
-      description: 'Descrição do Item 7',
-      image: 'sofa.jpg',
-  },
-  {
-      id: 8,
-      title: 'Item 8',
-      description: 'Descrição do Item 8',
-      image: 'sofa.jpg',
-  },
-  ];
+const produtoRepository = require('../../repository/produto');
 
-const produtoService = {
-    listarProdutos: () => produtos,
-    criarProduto: produto => {
-      const objetoProduto = JSON.parse(produto);
-      return `ProdutoService.criarProduto: ${objetoProduto.title}`;
-    },
-    atualizarProduto: produto => {
-      const objetoProduto = JSON.parse(produto);
-      return `ProdutoService.atualizarProduto: ${objetoProduto.title}`;
-    },
-    apagarProduto: produtoId => `ProdutoService.apagarProduto: ${produtoId}`,
+const listarProdutos = async () => await produtoRepository.listarProdutos();
+
+const criarProduto = produto => {
+  const objetoProduto = JSON.parse(produto);
+  const isTitle = Object.keys(objetoProduto).includes('title');
+
+  if (isTitle) {
+    return produtoRepository.criarProduto(objetoProduto);
+  }
+
+  return {
+    mensagem: 'Título não informado!'
+  };
+
 };
 
-module.exports = produtoService;
+const atualizarProduto = produto => {
+  const objetoProduto = JSON.parse(produto);
+  return produtoRepository.atualizarProduto(objetoProduto);
+};
+
+const apagarProduto = produtoId => produtoRepository.apagarProduto(produtoId);
+
+module.exports = {
+  listarProdutos,
+  criarProduto,
+  atualizarProduto,
+  apagarProduto,
+};
